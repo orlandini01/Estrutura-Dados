5dc3677bd47db572dbfa5a5017f7ca269b8cd1f0
PIERSILVIO DE CARVALHO ORLANDINI <156609726+orlandini01@users.noreply.github.com>
2025-02-24 15:48:05 -0300

    Add files via upload

---
+++ ProjetoPilha3/src/pilha/PilhaJavaUtil.java
@@ -0,0 +1,17 @@
+package pilha; // Certifique-se de que este nome corresponde ao nome do pacote criado no Eclipse
+
+import java.util.Stack;
+
+public class PilhaJavaUtil {
+    public static void main(String[] args) {
+        Stack<Integer> pilha = new Stack<>();
+
+        pilha.push(100);
+        pilha.push(200);
+        pilha.push(300);
+
+        System.out.println("Topo da pilha: " + pilha.peek());
+        System.out.println("Removendo: " + pilha.pop());
+        System.out.println("Pilha atual: " + pilha);
+    }
+}
