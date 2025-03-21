474064948e06a9d55703bd77bbd92d3a1980c181
PIERSILVIO DE CARVALHO ORLANDINI <156609726+orlandini01@users.noreply.github.com>
2025-03-21 14:03:20 -0300

    Add files via upload

---
+++ SistemaAluno/src/aplicacao/Main.java
@@ -0,0 +1,10 @@
+package aplicacao;
+
+import controle.ControlaAluno;
+
+public class Main {
+    public static void main(String[] args) {
+        ControlaAluno controle = new ControlaAluno();
+        controle.exibirMenu();
+    }
+}
